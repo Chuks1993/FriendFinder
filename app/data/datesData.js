b6b5ca70261ab26086e487d96ca4d1de284af693
Chuks1993 <chuksgrinage@gmail.com>
2018-02-27 15:55:47 -0600

    project

---
+++ app/data/datesData.js
@@ -0,0 +1,166 @@
+var datesArray = [
+  {
+  "name":"Matt Vella",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "male",
+  "type": "bad one",
+  "tier": 1,
+  "quote": ""
+},
+{
+  "name":"Jake Mire",
+  "photo":"",
+  "gender": "male",
+  "type": "take home to parents",
+  "tier": 2,
+  "quote":""
+},
+{
+  "name":"Sam Middleton",
+  "photo":"",
+  "gender": "male",
+  "type": "the perfect date",
+  "tier": 3,
+  "quote": ""
+},
+  {
+  "name":"Kendrick Davis",
+  "photo":"",
+  "gender": "male",
+  "type": "bad one",
+  "tier": 3,
+  "quote": ""
+},
+{
+  "name":"Ali Moedeli",
+  "photo":"",
+  "gender": "male",
+  "type": "take home to parents",
+  "tier": 2,
+  "quote":""
+},
+{
+  "name":"David Patel",
+  "photo":"",
+  "gender": "male",
+  "type": "the perfect date",
+  "tier": 2,
+  "quote": ""
+},
+  {
+  "name":"Liam Soh",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "male",
+  "type": "bad one",
+  "tier": 3,
+  "quote": ""
+},
+{
+  "name":"Craig Ojo",
+  "photo":"",
+  "gender": "male",
+  "type": "eccentric",
+  "tier": 2,
+  "quote":""
+},
+{
+  "name":"George Nguyen",
+  "photo":"",
+  "gender": "male",
+  "type": "eccentric",
+  "tier": 1,
+  "quote": ""
+},
+  {
+  "name":"Drake",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "male",
+  "type": "eccentric",
+  "tier": 2,
+  "quote": ""
+},
+{
+  "name":"Stephanie Lee",
+  "photo":"",
+  "gender": "female",
+  "type": "take home to parents",
+  "tier": 3,
+  "quote":""
+},
+{
+  "name":"Fatima Ali",
+  "photo":"",
+  "gender": "female",
+  "type": "the perfect date",
+  "tier": 1,
+  "quote": ""
+},
+  {
+  "name":"Matty Violla",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "female",
+  "type": "bad one",
+  "tier": 1,
+  "quote": ""
+},
+{
+  "name":"Joyce Mahr",
+  "photo":"",
+  "gender": "female",
+  "type": "take home to parents",
+  "tier": 3,
+  "quote":""
+},
+{
+  "name":"Ashleen Stacy",
+  "photo":"",
+  "gender": "female",
+  "type": "the perfect date",
+  "tier": 1,
+  "quote": ""
+},
+  {
+  "name":"Sanvi Reddy",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "female",
+  "type": "bad one",
+  "tier": 3,
+  "quote": ""
+},
+{
+  "name":"Sonya Ivanov",
+  "photo":"",
+  "gender": "female",
+  "type": "take home to parents",
+  "tier": 2,
+  "quote":""
+},
+{
+  "name":"Mary Schmidt",
+  "photo":"",
+  "gender": "female",
+  "type": "the perfect date",
+  "tier": 2,
+  "quote": ""
+},
+  {
+  "name":"Joclyn Espinoza",
+  "photo":"http://zeuswolf.com/images/takeoff.jpg",
+  "gender": "female",
+  "type": "eccentric",
+  "tier": 2,
+  "quote": ""
+},
+{
+  "name":"Natalie Hernandez",
+  "photo":"",
+  "gender": "female",
+  "type": "eccentric",
+  "tier": 2,
+  "quote":""
+}
+
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = datesArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,6 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
-var friendsData = require("../data/friendsData");
+
+var datesData = require("../data/datesData");
 
 
 
@@ -13,67 +9,36 @@ var friendsData = require("../data/friendsData");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+  app.get("/api/dates", function(req, res) {
+    res.json(datesData);
   });
 
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friendsdata array)
-  // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/dates", function(req, res) {
     
- // newFriend is the user that filled out the survey
-      var newFriend = req.body;
+ // newDate is the user that filled out the survey
+      var newDate = req.body;
+     
 
-      // compute best match from scores
-      var bestMatch = {};
+  
 
-      for(var i = 0; i < newFriend.scores.length; i++) {
-       
-          newFriend.scores[i] = parseInt(newFriend.scores[i]);
-        
-      }
-      // compare the scores of newFriend with the scores of each friend in the database and find the friend with the smallest difference when each set of scores is compared
+      bestMatch = {};
 
-      var bestMatchIndex = 0;
-      //greatest score difference for a question is 4, therefore greatest difference is 4 times # of questions in survey
-      var bestMatchDifference = 40;
-
-      for(var i = 0; i < friendsData.length; i++) {
-        var totalDifference = 0;
-
-        for(var index = 0; index < friendsData[i].scores.length; index++) {
-          var differenceOneScore = Math.abs(friendsData[i].scores[index] - newFriend.scores[index]);
-          totalDifference += differenceOneScore;
-        }
-
-        // if the totalDifference in scores is less than the best match so far
-        // save that index and difference
-        if (totalDifference < bestMatchDifference) {
-          bestMatchIndex = i;
-          bestMatchDifference = totalDifference;
-        }
+      for (var i = 0; i < datesData.length; i++) {
+        if (datesData[i].tier === parseInt(newDate.tier)) {
+          console.log(datesData[i]);
+          bestMatch = datesData[i];
+        } 
       }
 
-      // the best match index is used to get the best match data from the friends index
-      bestMatch = friendsData[bestMatchIndex];
+    
 
-      console.log(bestMatch);
-      // Put new friend from survey in "database" array
-      friendsData.push(newFriend);
+      
+    
+      datesData.push(newDate);
 
       // return the best match friend
       res.json(bestMatch);
@@ -85,9 +50,9 @@ module.exports = function(app) {
  
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    friendsData = [];
+    datesData = [];
     
 
-    console.log(friendsData);
+    console.log(datesData);
   });
 };
